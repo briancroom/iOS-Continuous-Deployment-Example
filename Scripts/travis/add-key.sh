8c131f36899a099acbf23d90d9a4d8031a8e4632
Brian Croom <bcroom@pivotal.io>
2015-11-24 21:51:39 -0500

    Fix key filename

--- Scripts/travis/add-key.sh
+++ Scripts/travis/add-key.sh
@@ -13,4 +13,4 @@ security -v set-keychain-settings -lut 86400 $KEYCHAIN
 # Download and import certificates and keys
 curl 'https://developer.apple.com/certificationauthority/DeveloperIDCA.cer' -o ./Scripts/travis/DeveloperIDCA.cer
 security import ./Scripts/travis/DeveloperIDCA.cer -k $KEYCHAIN -T /usr/bin/codesign
-security import ./Scripts/travis/DeveloperID.p12 -k $KEYCHAIN -P $SIGNING_KEY_PASSWORD -T /usr/bin/codesign
+security import ./Scripts/travis/developer.p12 -k $KEYCHAIN -P $SIGNING_KEY_PASSWORD -T /usr/bin/codesign
