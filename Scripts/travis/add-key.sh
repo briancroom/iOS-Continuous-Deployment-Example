349cc5e8af128bf6cb608c9cf7f82a3549404a46
Brian Croom <bcroom@pivotal.io>
2015-11-24 21:56:50 -0500

    Travis again

--- Scripts/travis/add-key.sh
+++ Scripts/travis/add-key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euf -o pipefail
+set -eu -o pipefail
 
 KEYCHAIN=ios-build.keychain
 
@@ -11,6 +11,11 @@ security unlock-keychain -p travis $KEYCHAIN
 security -v set-keychain-settings -lut 86400 $KEYCHAIN
 
 # Download and import certificates and keys
-curl 'https://developer.apple.com/certificationauthority/DeveloperIDCA.cer' -o ./Scripts/travis/DeveloperIDCA.cer
+curl 'https://developer.apple.com/certificationauthority/DeveloperIDCA.cer' -s -o ./Scripts/travis/DeveloperIDCA.cer
 security import ./Scripts/travis/DeveloperIDCA.cer -k $KEYCHAIN -T /usr/bin/codesign
-security import ./Scripts/travis/developer.p12 -k $KEYCHAIN -P $SIGNING_KEY_PASSWORD -T /usr/bin/codesign
+security import ./Scripts/travis/developer.p12 -k $KEYCHAIN -P "$SIGNING_KEY_PASSWORD" -T /usr/bin/codesign
+
+# Put the provisioning profile where Xcode can find it
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+ls ./Scripts/travis
+cp ./Scripts/travis/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
