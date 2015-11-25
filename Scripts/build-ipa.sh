b9ac7b0e3ef9b616c251eef9ad77aafb5f0dca98
Brian Croom <bcroom@pivotal.io>
2015-11-25 08:40:49 -0500

    Use xcodebuild_wrap

--- Scripts/build-ipa.sh
+++ Scripts/build-ipa.sh
@@ -10,11 +10,12 @@ OUTPUT_DIR=./build
 ARCHIVE_PATH="$OUTPUT_DIR/Archives/$BUILD_NAME.xcarchive"
 IPA_PATH="$OUTPUT_DIR/$BUILD_NAME.ipa"
 DSYM_NAME="$BUILD_NAME.dSYM.zip"
+XCODEBUILD="./Scripts/xcodebuild_wrap.sh"
 
 # 1. Make the archive
-xcodebuild archive -project "$PROJECT_NAME" -scheme "$SCHEME" -archivePath "$ARCHIVE_PATH"
+$XCODEBUILD archive -project "$PROJECT_NAME" -scheme "$SCHEME" -archivePath "$ARCHIVE_PATH"
 # 2. Export the IPA
-xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH" -exportPath "$OUTPUT_DIR" -exportOptionsPlist "Scripts/export-options.plist"
+$XCODEBUILD -exportArchive -archivePath "$ARCHIVE_PATH" -exportPath "$OUTPUT_DIR" -exportOptionsPlist "Scripts/export-options.plist"
 mv "$OUTPUT_DIR/$(basename `find "$ARCHIVE_PATH" -name *.app` .app).ipa" "$IPA_PATH"
 # 3. Zip the dSYM
 cd "$ARCHIVE_PATH/dSYMS" && zip -r -9 "$DSYM_NAME" $(find . -name "*.dSYM") >/dev/null && mv "$DSYM_NAME" ../../../ && cd -
